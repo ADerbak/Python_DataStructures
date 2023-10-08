b3fe0dc1a37287651101ccb967c7f490a612569a
Andrew <andrewderbak@gmail.com>
2023-10-08 12:12:17 -0500

    Adds LinkedList class to LinkedList.py. Practices implementation with main.py

--- LinkedList.py
+++ LinkedList.py
@@ -1,4 +1,12 @@
+# Create a single node
 class Node:
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
+
+
+# Create a linked list class - a collection of nodes
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
---
+++ main.py
@@ -0,0 +1,7 @@
+from LinkedList import LinkedList, Node
+
+ll = LinkedList()
+ll.head = Node(3)
+print(ll.head.data)
+
+
